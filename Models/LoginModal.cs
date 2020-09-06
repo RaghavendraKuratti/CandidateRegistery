28ba3676ee1c6bc323e916e197852d43bb7e491d
RaghavendraKuratti <raghavendrakuratti2@gmail.com>
2020-09-06 19:49:41 +0530

    Add files via upload

---
+++ Models/LoginModal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeRegistery.Models
+{
+    public class LoginModal
+    {
+
+        [Required(ErrorMessage = "Please Enter Email.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter Password.")]
+        public string Password { get; set; }
+    }
+
+    public class PasswordModal
+    {
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Models/WebRoleProvider.cs
@@ -0,0 +1,69 @@
+﻿using EmployeeRegistery.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace EmployeeRegistery.Models
+{
+    public class WebRoleProvider : RoleProvider
+    {
+        private UserRegisteryEntities db = new UserRegisteryEntities();
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            return (from user in db.Users
+                                     join role in db.UserRoles on user.Id equals role.UserId
+                                     join type in db.Roles on role.RoleId equals type.Id
+                                     where user.Email == username
+                    select type.Name).ToArray();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
